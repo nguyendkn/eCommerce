7a0a8a435edca9d3b2b0d969c17d406b9264b61d
Dao Khoi Nguyen <nguyendk.on@gmail.com>
2023-09-06 16:53:15 +0700

    feat(success): Handler success response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,14 +2,17 @@
 
 const AccessService = require("../services/access.service");
 
+const { OK, CREATED } = require("../core/success.response");
+
 class AccessController {
   signUp = async (req, res, next) => {
-    try {
-      console.log(`[P]::signUp:`, req.body);
-      return res.status(201).json(await AccessService.signUp(req.body));
-    } catch (error) {
-      next(error);
-    }
+    new CREATED({
+      message: "Regiserted OK?",
+      metadata: await AccessService.signUp(req.body),
+      options: {
+        limit: 10,
+      },
+    }).send(res);
   };
 }
 
